ae7c13672d976f2a89b7ee22a3d01ecdc0312ebe
peachamomomo <morimotoai2004@gmail.com>
2026-08-30 04:32:14 +0900

    nuGetからシリアルポート通信のライブラリを取得(mainに結合時は反映させない方がいいかも　その場合下記ファイル以外の変更を破棄、ブランチ爆破します) 通信用スクリプトSerialPortManager.csを実装中

---
+++ Assets/Scripts/SerialPortManager.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.IO.Ports;
+using System.Security.Authentication;
+using UnityEngine;
+
+public enum UserChoice
+{
+    idle,
+    left,
+    right
+}
+
+public enum WindowType
+{
+    idle,
+    chewing,
+    notChewing
+}
+
+public class SerialPortManager : MonoBehaviour
+{
+    [Header("UserAnswer")]
+    public UserChoice userChoice = UserChoice.idle;
+    public bool isCorrect = false;
+
+    [Header("Serial Settings")]
+    public string portName1 = "COM4";  // Inspectorから変更
+    public string portName2 = "COM3";  // Inspectorから変更
+    public int baudRate = 9600;
+
+    SerialPort sp1, sp2;
+
+    // TODO
+    // add device parameter
+    [Header("WindowType")]
+    public WindowType windowType = WindowType.idle;
+
+    void Start()
+    {
+        sp1 = new SerialPort(portName1, baudRate);
+        sp1.Open();
+
+        sp2 = new SerialPort(portName2, baudRate);
+        sp2.Open();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnApplicationQuit()
+    {
+        if (this.IsAllSerialPortOpen())
+        {
+            string data = 
+                /*userChoice*/ 0 + ","
+                + /*isCorrect*/ 0 + "," 
+                
+                //TODO
+                //add Parameter
+                + /*windowType*/ 0
+                //e.t.c.
+
+                + "\n";
+
+            sp1.Write(data);
+            sp1.Close();
+            sp2.Write(data);
+            sp2.Close();
+        }
+    }
+
+    public void setUserChoice(UserChoice c)
+    {
+        try
+        {
+            userChoice = c;
+        }
+        catch (ArithmeticException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    private void receiveData(string message)
+    {
+        String[] data;
+        if (this.IsAllSerialPortOpen())
+        {
+            data = message.Split(new string[] { "\n" }, System.StringSplitOptions.None);
+
+            //TODO
+            //add receiving parameter by Arduino
+            windowType = (WindowType)Int32.Parse(data[2]);
+            //e.t.c.
+
+            try
+            {
+                Debug.Log(data[0]);
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogWarning(e.Message);
+            }
+        }
+    }
+
+    //send to Arduino
+    public void WriteData()
+    {
+        if (this.IsAllSerialPortOpen())
+        {
+            string data1 = 
+                (int)userChoice + "," 
+                + (isCorrect? 1 : 0) + "," 
+                
+                //TODO
+                //add Parameter
+                + (int)windowType
+                //e.t.c.
+
+                + "\n";
+
+            string data2 =
+                (int)userChoice + ","
+                + (isCorrect ? 1 : 0) + "," 
+                
+                //TODO
+                //add Parameter
+                + (int)windowType
+                //e.t.c.
+
+                + "\n";
+
+            sp1.Write(data1);
+            sp2.Write(data2);
+        }
+    }
+
+    public bool IsAllSerialPortOpen()
+    {
+        return (sp1 != null && sp1.IsOpen && sp2 != null && sp2.IsOpen);
+    }
+}
